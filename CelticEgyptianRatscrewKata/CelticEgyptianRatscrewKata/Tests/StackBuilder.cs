b1c1aaba587fe389726e295be0475a8a17a98a41
Adrian Banks <github@adrianbanks.co.uk>
2015-01-26 23:14:03 +0000

    Introduce a StackBuild for creating stacks of cards

---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/StackBuilder.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace CelticEgyptianRatscrewKata.Tests
+{
+    public class StackBuilder
+    {
+        private readonly List<Card> cards = new List<Card>();
+
+        public StackBuilder With(Rank rank, Suit suit)
+        {
+            cards.Add(new Card(suit, rank));
+            return this;
+        }
+
+        public Stack Build()
+        {
+            return new Stack(cards);
+        }
+    }
+}
