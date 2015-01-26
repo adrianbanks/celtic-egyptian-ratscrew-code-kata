be72664d8c1e2b2184b53fc2619091578726bb5c
Adrian Banks <github@adrianbanks.co.uk>
2015-01-26 23:21:42 +0000

    Add a failing tests for sandwich snaps

---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/SandwichSnapValidatorTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace CelticEgyptianRatscrewKata.Tests
+{
+    [TestFixture]
+    public sealed class SandwichSnapValidatorTests
+    {
+        [Test]
+        public void AStackIsNotSnappable_WhenItContainsNoCards()
+        {
+            var stack = new StackBuilder()
+                            .Build();
+            bool isSnappable = new SandwichSnapValidator().IsSnappable(stack);
+            Assert.That(isSnappable, Is.False);
+        }
+    }
+}
