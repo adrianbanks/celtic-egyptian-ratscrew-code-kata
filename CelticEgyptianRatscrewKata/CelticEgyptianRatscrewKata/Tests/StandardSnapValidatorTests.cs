6a6818ed1a26b4171199d34615999779648c6887
Adrian Banks <github@adrianbanks.co.uk>
2015-01-26 22:10:09 +0000

    First passing test

--- CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/StandardSnapValidatorTests.cs
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/StandardSnapValidatorTests.cs
@@ -14,4 +14,12 @@ namespace CelticEgyptianRatscrewKata.Tests
             Assert.That(isSnappable, Is.False);
         }
     }
+
+    public class StandardSnapValidator
+    {
+        public bool IsSnappable(Stack stack)
+        {
+            return false;
+        }
+    }
 }
