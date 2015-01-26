3030e9ad44183a5bdf24483d5ea79fd4da40b2fd
Adrian Banks <github@adrianbanks.co.uk>
2015-01-26 22:05:56 +0000

    First failing test

---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/StandardSnapValidatorTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace CelticEgyptianRatscrewKata.Tests
+{
+    [TestFixture]
+    public sealed class StandardSnapValidatorTests
+    {
+        [Test]
+        public void IsSnappable_ReturnsFalse_WhenTheStackIsEmpty()
+        {
+            var stack = new Stack(new Card[0]);
+            var snapValidator = new StandardSnapValidator();
+            bool isSnappable = snapValidator.IsSnappable(stack);
+            Assert.That(isSnappable, Is.False);
+        }
+    }
+}
