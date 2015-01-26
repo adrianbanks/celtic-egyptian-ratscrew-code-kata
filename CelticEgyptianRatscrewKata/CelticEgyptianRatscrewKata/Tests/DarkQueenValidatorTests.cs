b34fcf636a59a1f28d2c1bf5f469d0c860e39405
Adrian Banks <github@adrianbanks.co.uk>
2015-01-26 22:51:27 +0000

    Add a failing tests for Dark Queens

---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/DarkQueenValidatorTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace CelticEgyptianRatscrewKata.Tests
+{
+    [TestFixture]
+    public sealed class DarkQueenValidatorTests
+    {
+        [Test]
+        public void AStackIsNotSnappable_WhenItIsEmpty()
+        {
+            var stack = new Stack(new Card[0]);
+            bool isSnappable = new DarkQueenSnapValidator().IsSnappable(stack);
+            Assert.That(isSnappable, Is.False);
+        }
+    }
+}
