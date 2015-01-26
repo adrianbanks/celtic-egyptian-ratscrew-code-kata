95b9c933f5e862c5e89a0da92935709b8d6d8ace
Adrian Banks <github@adrianbanks.co.uk>
2015-01-26 22:54:51 +0000

    Fix the failing test

--- CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/DarkQueenValidatorTests.cs
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/DarkQueenValidatorTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Linq;
+using NUnit.Framework;
 
 namespace CelticEgyptianRatscrewKata.Tests
 {
@@ -11,6 +12,22 @@ namespace CelticEgyptianRatscrewKata.Tests
             var stack = new Stack(new Card[0]);
             bool isSnappable = new DarkQueenSnapValidator().IsSnappable(stack);
             Assert.That(isSnappable, Is.False);
+        }
+
+        [Test]
+        public void AStackIsSnappable_WhenItContainsOnlyTheQueenOfSpades()
+        {
+            var stack = new Stack(new[] {new Card(Suit.Spades, Rank.Queen) });
+            bool isSnappable = new DarkQueenSnapValidator().IsSnappable(stack);
+            Assert.That(isSnappable, Is.True);
+        }
+    }
+
+    public class DarkQueenSnapValidator
+    {
+        public bool IsSnappable(Stack stack)
+        {
+            return stack.Count() == 1;
         }
     }
 }
