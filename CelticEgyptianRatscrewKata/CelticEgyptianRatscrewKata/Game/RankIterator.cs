860836db191d90d384ee7c73bbd8944889483a90
Adrian Banks <adrian.banks@red-gate.com>
2015-03-20 15:58:21 +0000

    Add an iterator to go through the ranks

---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Game/RankIterator.cs
@@ -0,0 +1,26 @@
+﻿namespace CelticEgyptianRatscrewKata.Game
+{
+    public class RankIterator
+    {
+        private Rank currentRank = Rank.Ace;
+
+        public RankIterator(Rank startingRank = Rank.Ace)
+        {
+            currentRank = startingRank;
+        }
+
+        public Rank GetNext()
+        {
+            var thisRank = currentRank;
+            var next = currentRank + 1;
+
+            if (next > Rank.King)
+            {
+                next = Rank.Ace;
+            }
+
+            currentRank = next;
+            return thisRank;
+        }
+    }
+}
---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/RankIteratorTests.cs
@@ -0,0 +1,29 @@
+﻿using CelticEgyptianRatscrewKata.Game;
+using NUnit.Framework;
+
+namespace CelticEgyptianRatscrewKata.Tests
+{
+    [TestFixture]
+    public sealed class RankIteratorTests
+    {
+        [Test]
+        public void TestIteratorStartsAtAceAndIncrements()
+        {
+            var iterator = new RankIterator();
+            var first = iterator.GetNext();
+            Assert.That(first, Is.EqualTo(Rank.Ace));
+            var second = iterator.GetNext();
+            Assert.That(second, Is.EqualTo(Rank.Two));
+        }
+
+        [Test]
+        public void TestIteratorCyclesRoundCorrectly()
+        {
+            var iterator = new RankIterator(Rank.King);
+            var first = iterator.GetNext();
+            Assert.That(first, Is.EqualTo(Rank.King));
+            var second = iterator.GetNext();
+            Assert.That(second, Is.EqualTo(Rank.Ace));
+        }
+    }
+}
