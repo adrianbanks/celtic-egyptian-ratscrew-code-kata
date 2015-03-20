09f16591b6969a270f5a608b7db35f7e71539a9b
Adrian Banks <adrian.banks@red-gate.com>
2015-03-20 16:34:32 +0000

    Add some tests for the new rule

---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/SnapRules/CallOutSnapRuleTests.cs
@@ -0,0 +1,44 @@
+﻿using CelticEgyptianRatscrewKata.Game;
+using CelticEgyptianRatscrewKata.SnapRules;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace CelticEgyptianRatscrewKata.Tests.SnapRules
+{
+    [TestFixture]
+    public sealed class CallOutSnapRuleTests
+    {
+        [Test]
+        public void SnapIsInvalid_WhenTheStackIsEmpty()
+        {
+            var gameState = Substitute.For<IGameState>();
+            gameState.CurrentCalledOutRank.Returns(Rank.Ten);
+            var snapRule = new CallOutSnapRule(gameState);
+            var cards = Cards.Empty();
+            
+            Assert.That(snapRule.IsSnapValid(cards), Is.False);
+        }
+
+        [Test]
+        public void SnapIsInvalid_WhenTheTopCardDoesNotMatchTheCallOut()
+        {
+            var gameState = Substitute.For<IGameState>();
+            gameState.CurrentCalledOutRank.Returns(Rank.Ten);
+            var snapRule = new CallOutSnapRule(gameState);
+            var cards = new Cards(new[] {new Card(Suit.Clubs, Rank.Four)});
+            
+            Assert.That(snapRule.IsSnapValid(cards), Is.False);
+        }
+
+        [Test]
+        public void SnapIsValid_WhenTheTopCardMatchesTheCallOut()
+        {
+            var gameState = Substitute.For<IGameState>();
+            gameState.CurrentCalledOutRank.Returns(Rank.Four);
+            var snapRule = new CallOutSnapRule(gameState);
+            var cards = new Cards(new[] {new Card(Suit.Clubs, Rank.Four)});
+            
+            Assert.That(snapRule.IsSnapValid(cards), Is.True);
+        }
+    }
+}
